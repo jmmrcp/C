edbdefdf1c377f6a4d74939553922e7a97ddc823
José Martínez <jmmrcp@gmail.com>
2014-08-25 09:52:48 +0000

    Ejemplo de salida

---
+++ salida_cadena.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define MAXLON 80
+
+int main (void)
+{
+    char cadena [MAXLON+1] = "Una Cadena";
+    printf ("El valor de cadena es %s. \n", cadena);
+    
+    return 0;
+}
